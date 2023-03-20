1266c6d59f9f9d2144d28f0fd20910a2c65188c8
heschwerdt <henry.schwerdtner@web.de>
2023-03-20 14:22:00 +0100

    added bucket versioning

--- server/src/minio/minio.service.ts
+++ server/src/minio/minio.service.ts
@@ -1,7 +1,6 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { Client } from 'minio';
 import { MINIO_CONNECTION } from 'nestjs-minio';
-import { join } from 'path';
 
 const TRAINING_DATA_BUCKET_NAME = "training-data";
 const MODEL_BUCKET = "models"
@@ -9,14 +8,32 @@ const MODEL_BUCKET = "models"
 @Injectable()
 export class MinioService {
   constructor(@Inject(MINIO_CONNECTION) private readonly minioClient: Client) { }
-
+  private static initBucket(name: string, minioClient: Client) {
+    var versioningStateConfig = {Status:"Enabled"}
+    minioClient.bucketExists(name, (err, exists) => {
+      if (err) {
+        console.log(err);
+      }
+      if (!exists) {
+        minioClient.makeBucket(name, "eu-central-1", (err) => {
+          if (err) {
+            console.log("error creating bucket")
+          } else {
+            console.log("Created Bucket.")
+            minioClient.setBucketVersioning(name, versioningStateConfig, function (error){
+              if (error) {
+                return console.log(error)
+              }
+              console.log("Success")
+            })
+          }
+        })
+      }
+    })
+  }
   onModuleInit(): void {
-    this.minioClient.makeBucket(TRAINING_DATA_BUCKET_NAME, "eu-central-1", (err) => {
-      console.log("Error", err)
-    })
-    this.minioClient.makeBucket(MODEL_BUCKET, "eu-central-1", (err) => {
-      console.log("Error", err)
-    })
+    MinioService.initBucket(TRAINING_DATA_BUCKET_NAME, this.minioClient);
+    MinioService.initBucket(MODEL_BUCKET, this.minioClient);
   }
 
   async putTrainingCompressedTrainingData(filePath: string, metaData: any) {
